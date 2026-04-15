09ce15064724551a7852caad914a97c56208c611
leandro roggerone <ingrogger@gmail.com>
2026-04-14 21:01:55 -0300

    implements animationgroups manager

--- src/player/Player.ts
+++ src/player/Player.ts
@@ -6,12 +6,18 @@ import { PhysicState } from "./statemachines/PhysicState";
 import { AnimationStateMachine } from "./statemachines/AnimationState";
 import type { Scene, Vector3, AbstractMesh, AnimationGroup } from "@babylonjs/core";
 import { CameraController } from "./controllers/CameraController";
+import { AnimationGroupsManager } from "./managers/AnimationGroupsMaanger";
 
 export class Player {
+  //state
   private inputState = new InputState();
   private physicState = new PhysicState();
   private animationState = new AnimationStateMachine();
 
+  //managers
+  private animationGroupsManager: AnimationGroupsManager;
+
+  //controlllers
   private inputController = new InputController(this.inputState);
   private physicController: PhysicController;
   private animationController: AnimationController;
@@ -25,8 +31,13 @@ export class Player {
     meshYOffset = 0
   ) {
     this.physicController = new PhysicController(scene, startPosition, mesh, this.inputState, this.physicState, this.animationState);
-    this.animationController = new AnimationController(this.inputState, this.physicState, this.animationState, animationGroups);
-
+    this.animationGroupsManager = new AnimationGroupsManager(animationGroups, this.animationState);
+    this.animationController = new AnimationController(
+      this.inputState,
+      this.physicState,
+      this.animationState,
+      this.animationGroupsManager  // Cambiado
+    );
     if (meshYOffset !== 0) {
       this.physicController.setMeshYOffset(meshYOffset);
     }
--- src/player/controllers/AnimationController.ts
+++ src/player/controllers/AnimationController.ts
@@ -2,7 +2,7 @@ import { type AnimationGroup, AnimationEvent } from "@babylonjs/core";
 import { type InputState } from "../statemachines/InputState";
 import { type PhysicState } from "../statemachines/PhysicState";
 import { type AnimationStateValue, type AnimationStateMachine } from "../statemachines/AnimationState";
-import { clusteredLightingFunctions } from "@babylonjs/core/Shaders/ShadersInclude/clusteredLightingFunctions";
+import { AnimationGroupsManager } from "../managers/AnimationGroupsMaanger";
 
 export class AnimationController {
   private groups: Record<AnimationStateValue, AnimationGroup | undefined> = {
@@ -24,85 +24,16 @@ export class AnimationController {
     private inputState: InputState,
     private physicState: PhysicState,
     private animationState: AnimationStateMachine,
-    animationGroups: AnimationGroup[] = []
+    private animationGroupsManager: AnimationGroupsManager  // Cambiado
   ) {
 
-    this.stopAllAnimations(animationGroups);
-    this.mapAnimationGroups(animationGroups);
-    this.disposehUnusedAnimations(animationGroups);
-  }
-
-
-  private disposehUnusedAnimations(animationGroups: AnimationGroup[]): void {
-    const usedGroups = new Set<AnimationGroup>(
-      Object.values(this.groups).filter((group): group is AnimationGroup => group !== undefined)
-    );
-
-    animationGroups.forEach((group) => {
-      if (!usedGroups.has(group)) {
-        group.dispose();
-        //console.warn(`Disposed unused animation group: ${group.name}`);
-      }
-    });
-  }
-
-
-  private stopAllAnimations(animationGroups: AnimationGroup[]): void {
-    animationGroups.forEach((ag: AnimationGroup) => ag.stop());
-  }
-
-  /**
-   * Maps animation groups by name to the corresponding state
-   */
-  private mapAnimationGroups(animationGroups: AnimationGroup[]): void {
-    this.groups.idle = animationGroups.find((item) => item.name === "idle");
-    this.groups.walking = animationGroups.find((item) => item.name === "walking");
-    this.groups.walking_backwards = animationGroups.find((item) => item.name === "walking backwards");
-    this.groups.running = animationGroups.find((item) => item.name === "slow running");
-
-    //jump
-    this.groups.jumping = animationGroups.find((item) => item.name === "falling idle");
-    this.groups.falling_down = animationGroups.find((item) => item.name === "fall a loop");
-    this.groups.falling_to_crash = animationGroups.find((item) => item.name === "falling impact");
-
-    //land-crash
-    this.groups.crashing_flat = animationGroups.find((item) => item.name === "falling flat impact");
-    if(this.groups.crashing_flat){
-      this.groups.crashing_flat.onAnimationGroupLoopObservable.add(() => { this.animationState.blockingAnimationIsPlaying = false });
-      this.groups.crashing_flat.from = 20;
-    }
-
-    //safety landing
-    const landing_event = new AnimationEvent(60, () => {this.animationState.blockingAnimationIsPlaying = false}, true);
-    this.groups.landing_safety = animationGroups.find((item) => item.name === "falling to landing");
-    if(this.groups.landing_safety){
-      this.groups.landing_safety.from = 20;
-      this.groups.landing_safety.to = 65
-      this.groups.landing_safety.speedRatio = 1.5;
-    }
-
-    const landing_anim = this.groups.landing_safety?.targetedAnimations[0].animation;
-    landing_anim?.addEvent(landing_event);
-
-
-    //jumping impulse start and over
-    const event = new AnimationEvent(30, () => {
-      this.animationState.blockingAnimationIsPlaying = false;
-      this.animationState.current = "jump_impulse_is_over"
-    }, true);
-
-    this.groups.jump_impulse_starts = animationGroups.find((item) => item.name === "jumping");
-    if(this.groups.jump_impulse_starts){
-      this.groups.jump_impulse_starts.speedRatio = 1.5;
-    }
-    const anim = this.groups.jump_impulse_starts?.targetedAnimations[0].animation;
-    anim?.addEvent(event);
-
-    this.groups.jump_impulse_is_over = this.groups.jumping;
-
-    console.warn("Animation groups set:", Object.keys(this.groups).filter(k => this.groups[k as AnimationStateValue]));
 
   }
+
+
+
+
+
 
   update(): void {
 
@@ -166,12 +97,12 @@ export class AnimationController {
   private play(state: AnimationStateValue): void {
     if (this.animationState.current === state) return;
 
-    this.groups[state]?.stop();
+    this.animationGroupsManager.groups[state]?.stop();  // Cambiado
     // Update animation state machine
     this.animationState.setState(state);
 
     // Play the corresponding animation
-    const group = this.groups[state] ?? this.groups.idle;
+    const group = this.animationGroupsManager.groups[state] ?? this.animationGroupsManager.groups.idle;  // Cambiado
     if (group) group.start(true, group.speedRatio, group.from, group.to, true);
   }
 }
