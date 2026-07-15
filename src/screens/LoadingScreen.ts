a4a0287e6581ea8eba4504fa4ee3d244de5bda15
leandro roggerone <ingrogger@gmail.com>
2026-07-14 21:24:52 -0300

    gamepad on loading screen / press any button on gamepad

--- src/screens/LoadingScreen.ts
+++ src/screens/LoadingScreen.ts
@@ -123,7 +123,7 @@ export class LoadingScreen {
     });
 
     const prompt = document.createElement("p");
-    prompt.textContent = "PRESS ANY KEY TO CONTINUE (or doble TAB)";
+    prompt.textContent = "PRESS ANY KEY OR BUTTON TO CONTINUE (or doble TAB)";
     Object.assign(prompt.style, {
       fontSize: "0.75rem",
       letterSpacing: "0.3em",
